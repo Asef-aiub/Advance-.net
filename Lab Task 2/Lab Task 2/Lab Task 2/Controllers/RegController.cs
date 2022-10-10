763f853aa521f6de0c28f1561ceebe046ef0d8ec
Asef-aiub <87447084+Asef-aiub@users.noreply.github.com>
2022-10-10 23:29:13 +0600

    Lab Task 2

---
+++ Lab Task 2/Lab Task 2/Lab Task 2/Controllers/RegController.cs
@@ -0,0 +1,33 @@
+﻿using Lab_Task_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_Task_2.Controllers
+{
+    public class RegController : Controller
+    {
+        // GET: Reg
+        [HttpGet]
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Form(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Success", "Reg");
+            }
+            return View(student);
+        }
+        public ActionResult Success()
+        {
+            return View();
+        }
+    }
+}
